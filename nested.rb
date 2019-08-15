595eb129b77e9bbeabc605183a4e13b524d0eff1
Arlene George <aemg@live.com>
2019-08-15 23:53:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
         }
      }
 programmer_hash.unshift[:yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }]
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }]
     
 p programmer_hash    
 end
