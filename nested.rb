b65f4748cee6bb472ec7c721562e89c6ac034742
Arlene George <aemg@live.com>
2019-08-16 00:31:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,5 +133,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+p programmer_hash
+programmer_hash
 end
