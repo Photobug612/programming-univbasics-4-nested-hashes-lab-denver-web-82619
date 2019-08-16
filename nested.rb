ae39c10ee52573cbc105324c5291dc0506bbc36a
Arlene George <aemg@live.com>
2019-08-16 00:29:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,12 +106,17 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+   
      programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     p programmer_hash
+     programmer_hash
      
 end
 
+  #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     #alans_new_info = "GUI"
+     
 def adding_to_dennis
 	programmer_hash = 
  		{
